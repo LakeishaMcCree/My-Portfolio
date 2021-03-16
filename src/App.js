b4bc960c35968a6f771bc2e6e24ea960f416e41f
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2021-03-15 21:05:19 -0400

    added component and container folders to organize the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import About from './containers/About'
+import Blog from './containers/Blog'
+import Projects from './containers/Projects'
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+  render() {
+    return(
+      <Router>
+        <Switch>
+          <div className="App">
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/blog' component={Blog} />
+            <Route exact path='/projects' component={Projects} />
+          </div>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
